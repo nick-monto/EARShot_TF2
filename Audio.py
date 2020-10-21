5114520266f09e6c071e393dac6558546c949f91
Kevin Brown <kevin.s.brown@uconn.edu>
2020-10-21 15:14:58 -0700

    Removed imports
    Commented out an unused function in Feeder and removed imports that no longer worked after I edited Audio.py

--- Audio.py
+++ Audio.py
@@ -12,21 +12,28 @@ def preemphasis(x, preemphasis = 0.97):
 def inv_preemphasis(x, preemphasis = 0.97):
     return signal.lfilter([1], [1, -preemphasis], x)
 
-
-def spectrogram(y, num_freq, frame_shift_ms, frame_length_ms, sample_rate, ref_level_db = 20):    
-    D = _stft(preemphasis(y), num_freq, frame_shift_ms, frame_length_ms, sample_rate)    
+def spectrogram(y, dimension, frame_shift, frame_len, samp_rate, ref_level_db = 20):
+    '''
+    Computes a spectogram with dimension frequencies from a time series/audio signal.
+    '''
+    D = _stft(preemphasis(y), dimension, frame_shift, frame_len, samp_rate)
     S = _amp_to_db(np.abs(D)) - ref_level_db
     return _normalize(S)
+
+def mel_spectrogram(y, spec_dimension, frame_shift, frame_len, dimension, samp_rate, max_abs = None):
+    '''
+    Computes a mel spectogram with spec_dimension resolution and dimension mel components
+    from a time series/audio signal.
+    '''
+    D = _stft(preemphasis(y), spec_dimension, frame_shift, frame_len, samp_rate)
+    S = _amp_to_db(_linear_to_mel(np.abs(D), spec_dimension, dimension, samp_rate))
+    return _normalize(S) if max_abs is None else _symmetric_normalize(S, max_abs_value= max_abs)
 
 def inv_spectrogram(spectrogram, num_freq, frame_shift_ms, frame_length_ms, sample_rate, ref_level_db = 20, power = 1.5, griffin_lim_iters= 60):
     '''Converts spectrogram to waveform using librosa'''
     S = _db_to_amp(_denormalize(spectrogram) + ref_level_db)  # Convert back to linear
     return inv_preemphasis(_griffin_lim(S ** power, num_freq, frame_shift_ms, frame_length_ms, sample_rate, griffin_lim_iters= griffin_lim_iters))          # Reconstruct phase
 
-def melspectrogram(y, num_freq, frame_shift_ms, frame_length_ms, num_mels, sample_rate, max_abs_value = None):
-    D = _stft(preemphasis(y), num_freq, frame_shift_ms, frame_length_ms, sample_rate)
-    S = _amp_to_db(_linear_to_mel(np.abs(D), num_freq, num_mels, sample_rate))
-    return _normalize(S) if max_abs_value is None else _symmetric_normalize(S, max_abs_value= max_abs_value)
 
 def _griffin_lim(S, num_freq, frame_shift_ms, frame_length_ms, sample_rate, griffin_lim_iters = 60):
     '''librosa implementation of Griffin-Lim
@@ -48,7 +55,7 @@ def _istft(y, num_freq, frame_shift_ms, frame_length_ms, sample_rate):
     _, hop_length, win_length = _stft_parameters(num_freq, frame_shift_ms, frame_length_ms, sample_rate)
     return librosa.istft(y, hop_length=hop_length, win_length=win_length)
 
-def _stft_parameters(num_freq, frame_shift_ms, frame_length_ms, sample_rate):    
+def _stft_parameters(num_freq, frame_shift_ms, frame_length_ms, sample_rate):
     n_fft = (num_freq - 1) * 2
     hop_length = int(frame_shift_ms / 1000 * sample_rate)
     win_length = int(frame_length_ms / 1000 * sample_rate)
@@ -62,7 +69,6 @@ def _linear_to_mel(spectrogram, num_freq, num_mels, sample_rate):
 def _build_mel_basis(num_freq, num_mels, sample_rate):
     n_fft = (num_freq - 1) * 2
     return librosa.filters.mel(sample_rate, n_fft, n_mels=num_mels)
-
 
 def _amp_to_db(x):
     return 20 * np.log10(np.maximum(1e-5, x))
