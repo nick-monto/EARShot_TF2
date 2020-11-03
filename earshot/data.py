6235995edbd3826b1976497efcccefc75b5fb944
Kevin Brown <kevin.s.brown@uconn.edu>
2020-11-02 17:44:31 -0800

    Multiple updates
    +padding function added (initial commit of data.py)
    +model tests added

---
+++ earshot/data.py
@@ -0,0 +1,12 @@
+from keras.models import Sequential
+from numpy import ones
+
+def pad(data_2d, ref_shape, pad_val=0.0):
+    '''
+    Pads a 2D matrix "data" with the pad_value to make it have shape ref_shape.
+    All the padded values occur "off the end" of the data array, so that the
+    original array occupies the upper left of the resulting matrix.
+    '''
+    padded_data = pad_val*ones(ref_shape)
+    padded_data[:data_2d.shape[0],:data_2d.shape[1]] = data_2d
+    return padded_data
