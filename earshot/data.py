c29ea02be5f3dfe79cc3f7ceca71ff6f949b0a11
nicholas <monto.nicholas@gmail.com>
2020-11-13 17:28:10 -0500

    Added new data class and moved training example.

--- earshot/data.py
+++ earshot/data.py
@@ -1,17 +1,18 @@
-import enum
 import librosa
+import math
 import os
 import pandas as pd
 import numpy.matlib as npm
+import matplotlib.pyplot as plt
 
 from earshot.audio import *
 from numpy import ones
 from scipy import sparse
+from scipy import fftpack
 from tensorflow.keras.utils import Sequence
 from tqdm import tqdm
 
-# TODO include pass for precomputed input
-
+# TODO include a pass for precomputed input to the Batch Generator / Manifest
 
 def pad(data_2d, ref_shape, pad_val=-9999):
     '''
@@ -67,7 +68,7 @@ class Manifest(object):
         print("Number of unique word utterances by each talker.")
         print(self.manifest.groupby('Talker').count()['Word'])
 
-    def generate_srvs(self, target='Talker', target_len=300, target_on=10):
+    def generate_srvs(self, target='Word', target_len=300, target_on=10):
         '''
         target = manifest column containing desired targets
         target_len = desired length of target vector
@@ -140,20 +141,98 @@ class DataGenerator(Sequence):
         # pad all specs in batch to max length
         padded_spec = [ pad(s, (M, s.shape[1]), pad_val=self.pad_value)
                        for s in spec ]
-        X = np.empty((self.batch_size, M, spec[0].shape[1]))
+        X = np.empty((self.batch_size, M, spec[0].shape[1]), dtype=np.float32)
         # pad targets if LSTM layer has return_sequence=True
         if self.return_seq:
-            y = np.empty((self.batch_size, M, len(list_pairs_temp[0][1])), dtype=int)
+            y = np.empty((self.batch_size, M, len(list_pairs_temp[0][1])), dtype=np.float32)
             targets = [ npm.repmat(pair[1],spec[i].shape[0],1) for i,pair in enumerate(list_pairs_temp) ]
             padded_targets = [ pad(i,(M,i.shape[1]),pad_val=self.pad_value) for i in targets ]
             for i, pair in enumerate(list_pairs_temp):
                 X[i, ] = padded_spec[i]
                 y[i, ] = padded_targets[i]
         else:
-            y = np.empty((self.batch_size, len(list_pairs_temp[0][1])), dtype=int)
+            y = np.empty((self.batch_size, len(list_pairs_temp[0][1])), dtype=np.float32)
 
             for i, pair in enumerate(list_pairs_temp):
                 X[i, ] = padded_spec[i]
                 y[i] = pair[1]
 
         return X, y
+
+
+class AudioTools(object):
+    '''
+    A suite of tools to generate SPL spectrograms for a specified audio file.
+
+    The reference for the majority of these functions can be found here:
+    https://courses.engr.illinois.edu/ece590sip/sp2018/spectrograms1_wideband_narrowband.html
+
+    '''
+    def __init__(self, audiopath):
+        '''
+        audiopath = path to desired audio
+        '''
+        sig, self.fs = librosa.core.load(audiopath)
+        self.signal = librosa.effects.trim(
+            sig, frame_length=32, hop_length=16)[0]
+
+    def _enframe(self, signal, skip_len, window_len):
+        # w = 0.54*np.ones(L)
+        # for n in range(0,L):
+        #   w[n] = w[n] - 0.46*math.cos(2*math.pi*n/(L-1))
+        w = np.hamming(window_len)
+        frames = []
+        nframes = 1+int((len(signal)-window_len)/skip_len)
+        for t in range(0, nframes):
+            frames.append(
+                np.copy(signal[(t*skip_len):(t*skip_len+window_len)])*w)
+        return frames
+
+    def _stft(self, frames, n_fft, Fs):
+        stft_frames = [fftpack.fft(x, n_fft) for x in frames]
+        freq_axis = np.linspace(0, Fs, n_fft)
+        return(stft_frames, freq_axis)
+
+    def _stft2level(self, stft_spectra, max_freq_bin):
+        magnitude_spectra = [np.abs(x) for x in stft_spectra]
+        max_magnitude = max([max(x) for x in magnitude_spectra])
+        min_magnitude = max_magnitude / 1000.0
+        for t in range(0, len(magnitude_spectra)):
+            for k in range(0, len(magnitude_spectra[t])):
+                magnitude_spectra[t][k] /= min_magnitude
+                if magnitude_spectra[t][k] < 1:
+                    magnitude_spectra[t][k] = 1
+        # convert to SPL spectra
+        level_spectra = [20*np.log10(x[0:max_freq_bin])
+                         for x in magnitude_spectra]
+        return level_spectra
+
+    def sgram(self, window_len, skip_len, max_freq):
+        '''
+        window_len: length of window in seconds
+        skip_len: length to skip in seconds
+        max_freq: maximum desired frequency
+        '''
+        self.n_fft = pow(2, int(math.log(int(self.fs*window_len),
+                                         2) + 0.5))  # calc NFFT suitable for window_len
+        window_len = int(window_len*self.fs)  # convert to length in samples
+        skip_len = int(skip_len*self.fs)  # convert to length in samples
+        self.frames = self._enframe(self.signal, skip_len, window_len)
+        spectra, self.freq_axis = self._stft(self.frames, self.n_fft, self.fs)
+        self.spl_spec = np.array(self._stft2level(
+            spectra, int(max_freq*self.n_fft/self.fs)))
+        self.max_time = len(self.frames)*skip_len/self.fs
+        self.max_freq = max_freq
+        return self.spl_spec
+
+    def plot_spec(self):
+        try:
+            plt.imshow(self.spl_spec.transpose(),
+                       origin='lower',
+                       extent=(0, self.max_time, 0, self.max_freq),
+                       aspect='auto')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Frequency (Hz)')
+            plt.show()
+        except:
+            print("Please run the sgram method before plotting.")
---
+++ earshot/example-scripts/train_example.py
@@ -0,0 +1,40 @@
+from earshot.audio import *
+import numpy as np
+from scipy import spatial
+import tensorflow as tf
+from earshot.data import *
+from earshot.model import EARSHOT
+from earshot.parameters import ModelParameters
+
+# log desired audio files, provide path to folder containing them
+train_manifest = Manifest('/home/nicholas/Documents/EARSHOT_NEW_WAVS')
+
+# generate target SRVs
+train_manifest.generate_srvs(target='Word', target_len=300, target_on=10)
+
+# create batch generator of 500 items from the input manifest excluding the talker Junior
+# remove the .sample() to create a batch generator of the whole set
+batch_gen = DataGenerator(df=train_manifest.manifest[train_manifest.manifest.Talker != 'JUNIOR'].sample(500),
+                          batch_size=16, pad_value=-9999, return_seq=True)
+
+p = ModelParameters()
+
+# instantiate model
+# sub-classed model requires length of target vector and model parameters
+model = EARSHOT(train_manifest.manifest['Target'][0].shape[0], p)
+model.compile(loss=model.loss, optimizer="adam", metrics=[
+              tf.keras.metrics.CategoricalAccuracy()])
+model.fit(
+    batch_gen,
+    epochs=5,
+    shuffle=True,
+    use_multiprocessing=False
+)
+
+# Prediction example
+test = spectro_calc(train_manifest.manifest['Path'].tolist()[0])
+test_prediction = model.predict(test.reshape(1, -1, 256))
+
+results = [1 - spatial.distance.cosine(train_manifest.manifest['Target'].tolist()[
+                                       0], i) for i in test_prediction[0]]
+np.max(results)
